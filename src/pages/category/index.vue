8ca6f7f4df9b640628a69904ec26783febd4ebbf
LiRongZhi <1752146907@qq.com>
2019-09-26 21:05:27 +0800

    我的页面排版

---
+++ src/pages/category/index.vue
@@ -0,0 +1,49 @@
+<template>
+	<view class="content">
+		分类
+	</view>
+</template>
+
+<script>
+	import mixins from '../../common/mixin';
+	import util from '../../common/util';
+
+	export default {
+        mixins: [mixins],
+		data() {
+			return { 
+				pageData: {}
+			}
+		},
+		onLoad() {
+			this.handleLoad();
+		},
+		methods: {
+			handleLoad: function() {
+				this.request({
+					url: 'api/page',
+					method: 'GET',
+					data: {
+
+					},
+					success: (response) => {
+						this.pageData = response;
+					},
+					fail: (response) => {
+
+					},
+					complete: () => {
+
+					}
+				});
+			} 
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	} 
+</style>
