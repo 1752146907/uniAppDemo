d9ad760f1d509f46eb0cfde57dfa91ff5ca8fa84
LiRongZhi <1752146907@qq.com>
2019-09-30 14:46:20 +0800

    路由

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import Vue from 'vue'
 import App from './App'
 import storage from './common/storage.js' 
+import Router from 'uni-simple-router'  
+import pagesRouter from './pages.json'
 
 Vue.config.productionTip = false
 
 Vue.prototype.$storage = storage;				// 本地存储
+
+Vue.use(Router)
+
+const router = new Router({
+	loading:false,
+	routes: []
+});
+
+Router.beforeEach((to, from, next) => {
+	// 一些逻辑
+})
 
 App.mpType = 'app'
 
