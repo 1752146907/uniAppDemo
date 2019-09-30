2010b8e7be8f34b337984fd03ee64064f0e3672a
LiRongZhi <1752146907@qq.com>
2019-09-30 19:17:24 +0800

    集成cavas2html

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -1,49 +1,52 @@
 <template>
 	<view class="content">
 		分类
+		<view id="capture">
+			<view style="padding: 10px; background: #f5da55"><h4 style="color: #000; " @click="toImage">Hello world!</h4></view>
+			<view @click="handleLoad">下载</view>
+			<image :src="url" alt="" />
+			<view ref="canvasContain"></view>
+		</view>
 	</view>
 </template>
 
 <script>
-	import mixins from '../../common/mixin';
-	import util from '../../common/util';
+import mixins from '../../common/mixin';
+import util from '../../common/util';
+import html2canvas from 'html2canvas';
 
-	export default {
-        mixins: [mixins],
-		data() {
-			return { 
-				pageData: {}
-			}
-		},
-		onLoad() {
-			this.handleLoad();
-		},
-		methods: {
-			handleLoad: function() {
-				this.request({
-					url: 'api/page',
-					method: 'GET',
-					data: {
-
-					},
-					success: (response) => {
-						this.pageData = response;
-					},
-					fail: (response) => {
-
-					},
-					complete: () => {
-
-					}
+export default {
+	mixins: [mixins],
+	data() {
+		return {
+			dataURL:0,
+			pageData: {},
+			url: ''
+		};
+	},
+	onLoad() {
+		
+	},
+	methods: {
+		handleLoad: function() {
+			let _this = this;
+			html2canvas(document.getElementById('capture')).then(function(canvas) {
+				// 打印临时URL
+				_this.url = canvas.toDataURL("image/png");
+				console.log(canvas.toDataURL("image/png"))
+				uni.showToast({
+				    title: '截图了',
+				    duration: 2000
 				});
-			} 
+			});
 		}
 	}
+};
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	} 
+.content {
+	text-align: center;
+	height: 400upx;
+}
 </style>
