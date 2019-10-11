7d13a7f5a02c0318eab01da6f295efb61f62657e
LiRongZhi <1752146907@qq.com>
2019-10-11 15:04:41 +0800

    cavas保存为图片

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -1,6 +1,10 @@
 <template>
 	<view class="content">
-		分类 
+		<!-- 这里是状态栏 -->
+		<view class="status_bar"></view> 
+		<view>分类 </view>
+		<button @click="handleSeverCavasToImage">保持cavas为图片</button>
+		<canvas canvas-id="myCanvas" :style="'width:'+windowWidth+'px;height:'+windowHeight+'px'"/>
 	</view>
 </template>
 
@@ -14,12 +18,55 @@ export default {
 		return {
 			dataURL: 0,
 			pageData: {},
-			url: ''
+			url: '',
+			windowWidth: 278,
+			windowHeight: 524
 		};
 	},
 	onLoad() {},
 	methods: {
-		 
+		handleSeverCavasToImage: function () {
+			uni.showLoading({
+				title: '图片绘制中...',
+			})
+			var url = '../../static/goods_canvas04.png'
+			 
+			//开始绘制图片，这边主要就是canvas写法了，uniapp官方：https://uniapp.dcloud.io/api/ui/canvas?id=%E5%9C%A8canvas%E4%B8%8A%E7%94%BB%E5%9B%BE
+			const context = uni.createCanvasContext('myCanvas')  
+			context.drawImage(url,0, 0, this.windowWidth, this.windowHeight) 
+			context.setFontSize(14)
+			context.setFillStyle("#FEEDBB")
+			context.fillText('李荣浩', 118, 60)
+			
+			//重点：这边本来保存图片是写在draw之后，但第一次保存时空白，第二次才生效，写在draw回调里面就OK了。
+			context.draw(false,function(){
+				uni.canvasToTempFilePath({
+					canvasId:'myCanvas',
+					success: function(res){
+						uni.hideLoading()
+						console.log(res.tempFilePath)
+						
+						//#ifdef H5
+						let imageList = [];
+						imageList.push(res.tempFilePath)
+						uni.previewImage({
+							urls: imageList,
+						})
+						//#endif
+						
+						//#ifdef MP-WEIXIN
+							uni.saveImageToPhotosAlbum({
+								filePath:res.tempFilePath,
+								success : function(res){
+									uni.showToast({title : '图片已保存'})
+								}
+							})
+						//#endif
+					}
+				})
+				
+			}) 
+		}
 	}
 };
 </script>
